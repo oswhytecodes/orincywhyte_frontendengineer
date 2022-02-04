29c321d045982291e030c228cd2198da1ed6b96c
Orincy Whyte <orincywhyte@gmail.com>
2022-02-04 12:37:10 -0500

    files added

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: [
+    './public/**/*.{html,js}',
+  ],
+  theme: {
+   
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+    screens: {
+      lg: '1124px',
+      xl: '1124px',
+      '2xl': '1124px',
+    },
+    fontFamily: {
+      cursive: ['Sacramento', 'cursive'],
+      sans: ['Montserrat', 'sans-serif'],
+    },
+    colors: {
+      'accentgreen': '#66da42',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
